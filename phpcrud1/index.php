c589e667fe95f4ed9fb7e6b387dcf4efbe27222d
mh-fci <maruf.hossaiin@gmail.com>
2019-05-24 21:58:09 +0600

    create index page for crud operation

---
+++ phpcrud1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome PHP Crud Operation</title>
+    <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+          <form class="" action="" method="post">
+            <div class="form-group">
+              <label >First Name</label>
+              <input type="text" class="form-control" name="fname" value="">
+              <label>Last Name</label>
+              <input type="text" class="form-control" name="lname" value="">
+              <label>School</label>
+              <input type="text" class="form-control" name="schoolname" value="">
+              <label>Passing Year</label>
+              <input type="text" class="form-control" name="passingyear" value="">
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-info" name="save">SAVE</button>
+            </div>
+          </form>
+      </div>
+    </div>
+
+  </body>
+</html>
